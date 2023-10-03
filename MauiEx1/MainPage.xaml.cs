944dd878ad90bf5ca927cf4b8fb59735cfde8be2
PrunierPro <prunierthomas.pro@gmail.com>
2023-10-03 16:50:52 +0200

    Add project files.

---
+++ MauiEx1/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiEx1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiExo1/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MauiExo1
+{
+    public partial class MainPage : ContentPage
+    {
+        int number = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            number = GenerateMagicNumber();
+        }
+
+        private int GenerateMagicNumber()
+        {
+            Random rand = new Random();
+            return rand.Next(21);
+        }
+
+        private void Guess(object sender, EventArgs e)
+        {
+            MessageLabel.SetValue(Label.TextColorProperty, Colors.White);
+            int num = 0;
+            try
+            {
+                num = int.Parse(((Entry)sender).Text);
+                if (num < 1 || num > 20) throw new FormatException();
+            }
+            catch
+            {
+                MessageLabel.SetValue(Label.TextColorProperty, Colors.Red);
+                MessageLabel.SetValue(Label.TextProperty, "You need to enter a number between 1 and 20.");
+                return;
+            }
+            if (num == number)
+            {
+                MessageLabel.SetValue(Label.TextProperty, "Congratulations! You found the number!");
+                MessageLabel.SetValue(Label.TextColorProperty, Colors.Green);
+            }
+            else
+            {
+                MessageLabel.SetValue(Label.TextColorProperty, Colors.Red);
+                MessageLabel.SetValue(Label.TextProperty, "That wasn't the right number. Try again.");
+            }
+        }
+    }
+}
